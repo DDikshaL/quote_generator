30d15db850e600b813fd1066670e75b42c29bd26
Diksha Dedhiya <dedhiyadiksha@gmail.com>
2022-07-06 15:50:44 +0530

    added files

---
+++ script.js
@@ -0,0 +1,99 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteButton = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
+
+
+let apiQuotes = [];
+
+//Show Loading
+
+function loading()
+{
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+//Hide Loading
+
+function complete()
+{
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+}
+
+
+
+function newQuote()
+{
+
+    loading();
+    //Pick a random quote from API array
+   
+    const quote = apiQuotes[Math.floor(Math.random() *  apiQuotes.length)];
+    console.log(quote);
+
+    //Check if author field is blank and replace it with 'Unknown'
+    if (quote.author == null)
+    {
+        authorText.textContent = 'Unknown';  
+    }
+    else
+    {
+        authorText.textContent = quote.author;
+    }
+
+    //Check quote length to apply styling from css class
+
+    if(quote.text.length > 120)
+    {
+         quoteText.classList.add('long-quote');
+    } else
+    {
+         quoteText.classList.remove('long-quote');
+    }
+    
+    //Set quote, Hide Loader
+    quoteText.textContent = quote.text;
+    complete();
+}
+
+// Tweet Quote
+
+function tweetQuote()
+{
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+    window.open( twitterUrl, '_blank');
+}
+
+//Event Listener
+newQuoteButton.addEventListener('click',newQuote);
+twitterBtn.addEventListener('click', tweetQuote);
+
+
+// Get Quotes from API
+
+async function getQuotes()
+{
+    loading();
+    const apiURL='https://type.fit/api/quotes';
+    try
+    {
+         const response = await fetch(apiURL);
+         apiQuotes = await response.json();
+         newQuote();
+    }
+
+    catch (error)
+    {
+        // Catch error here
+    }
+    
+}
+
+//OnLoad
+
+getQuotes();
+
